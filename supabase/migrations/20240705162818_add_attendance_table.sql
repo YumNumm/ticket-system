1391db5eba75506bf96d0ccd186b0425067b973d
Restyled.io <commits@restyled.io>
2024-07-05 19:45:39 +0000

    Restyled by pg_format

--- supabase/migrations/20240705162818_add_attendance_table.sql
+++ supabase/migrations/20240705162818_add_attendance_table.sql
@@ -1,34 +1,46 @@
-CREATE TYPE attendance_status AS ENUM ('PRESENT', 'LEAVE');
+CREATE TYPE attendance_status AS ENUM (
+    'PRESENT',
+    'LEAVE'
+);
 
 CREATE TABLE public.attendances (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v7(),
-  user_id UUID NOT NULL REFERENCES auth.users ON DELETE CASCADE,
-  status attendance_status NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v7 (),
+    user_id uuid NOT NULL REFERENCES auth.users ON DELETE CASCADE,
+    status attendance_status NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
 );
 
 ALTER TABLE public.attendances ENABLE ROW LEVEL SECURITY;
 
-CREATE INDEX ON public.attendances(user_id);
-CREATE INDEX ON public.purchases(user_id);
+CREATE INDEX ON public.attendances (user_id);
 
+CREATE INDEX ON public.purchases (user_id);
 
-CREATE TYPE public.role AS ENUM ('admin', 'user', 'sponsor', 'speaker');
-ALTER TABLE public.profiles ADD COLUMN role role DEFAULT 'user' NOT NULL;
+CREATE TYPE public.role AS ENUM (
+    'admin',
+    'user',
+    'sponsor',
+    'speaker'
+);
 
-CREATE OR REPLACE FUNCTION public.role()
-RETURNS role
-STABLE
-AS $$
-  SELECT role
-  FROM public.profiles
-  WHERE id = auth.uid();
-$$ LANGUAGE sql
-SECURITY DEFINER
-SET search_path TO 'public';
+ALTER TABLE public.profiles
+    ADD COLUMN ROLE ROLE DEFAULT 'user' NOT NULL;
 
+CREATE OR REPLACE FUNCTION public.role ()
+    RETURNS ROLE STABLE
+    AS $$
+    SELECT
+        ROLE
+    FROM
+        public.profiles
+    WHERE
+        id = auth.uid ();
 
+$$
+LANGUAGE sql
+SECURITY DEFINER SET search_path TO 'public';
 
 CREATE POLICY "Only admin can ALL on attendances" ON public.attendances
-FOR ALL
-USING (role() = 'admin');
+    FOR ALL
+        USING (ROLE () = 'admin');
+
--- supabase/migrations/20240705174446_allow_admin_to_select.sql
+++ supabase/migrations/20240705174446_allow_admin_to_select.sql
@@ -1,9 +1,8 @@
 CREATE POLICY "Only admin can SELECT on profiles" ON public.profiles
-FOR SELECT
-TO authenticated
-USING (role() = 'admin');
+    FOR SELECT TO authenticated
+        USING (ROLE () = 'admin');
 
 CREATE POLICY "Only admin can SELECT on purchases" ON public.purchases
-FOR SELECT
-TO authenticated
-USING (role() = 'admin');
+    FOR SELECT TO authenticated
+        USING (ROLE () = 'admin');
+
